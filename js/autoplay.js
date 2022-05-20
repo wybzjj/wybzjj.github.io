fd4c5c712a56f4e32c60451b61d22acfaea20715
wybzjj <105898171+wybzjj@users.noreply.github.com>
2022-05-20 10:29:31 +0800

    Create autoplay.js

---
+++ js/autoplay.js
@@ -0,0 +1,8 @@
+document.addEventListener('click', () => {
+    document.querySelector("#audio").play();
+    // alert("特别的纪念送给特别的你");
+});
+
+document.querySelector(".quxiao").addEventListener("click", function() {
+    document.querySelector(".mask").style.display = "none";
+})
